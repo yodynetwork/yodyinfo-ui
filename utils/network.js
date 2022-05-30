b4fc8455f6f0b8293e6018b3db8a18d09d996c6d
Yody Network <yodynetwork@gmail.com>
2022-05-30 07:33:02 +0700

    Update

--- utils/network.js
+++ utils/network.js
@@ -3,14 +3,14 @@ export default {
     pubkey: 0x3a,
     pubkeyhash: 0x3a,
     scripthash: 0x32,
-    witness_v0_keyhash: 'qc',
-    witness_v0_scripthash: 'qc'
+    witness_v0_keyhash: 'yc',
+    witness_v0_scripthash: 'yc'
   },
   testnet: {
     pubkey: 0x78,
     pubkeyhash: 0x78,
     scripthash: 0x6e,
-    witness_v0_keyhash: 'tq',
-    witness_v0_scripthash: 'tq'
+    witness_v0_keyhash: 'ty',
+    witness_v0_scripthash: 'ty'
   }
 }[process.env.network || 'mainnet']
